9b629b8d9be4e8112bdf3dbb87890613aa2c9f19
Oleksandr Ryzhkov <ryzhman@gmail.com>
2018-04-25 23:59:56 -0400

    Redis server is started from main() method

--- Formula1APIProvider/src/main/com/formula1/standings/ApplicationRunner.java
+++ Formula1APIProvider/src/main/com/formula1/standings/ApplicationRunner.java
@@ -1,19 +1,26 @@
 package com.formula1.standings;
 
+import com.formula1.standings.utils.RedisRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by Oleksandr Ryzhkov on 25.03.2017.
  */
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ApplicationRunner {
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initRedisServer(RedisRunner redisRunner) {
+        return args -> redisRunner.runRedis();
     }
 }
